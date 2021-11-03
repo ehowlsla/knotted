0303551f06d52941da0f79a4115e217e1ed2718b
ehowlsla <rupy1014@gmail.com>
2021-11-03 23:59:45 +0900

    예약 진행중

--- lib/page/home/tab2.dart
+++ lib/page/home/tab2.dart
@@ -15,29 +15,113 @@ class _Tab2State extends State<Tab2> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      color: NColors.powderPink2,
-      child: SafeArea(
-        child: Container(
-          color: Colors.white,
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints viewportConstraints) {
-              return SingleChildScrollView(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                      minHeight: viewportConstraints.maxHeight
-                  ),
-                  child: Column(
-                    children: [
-                      HomeUserInfo(),
-                      BestMenu(),
-                      NoticeCarousel(),
-                      EventCarousel(),
-                    ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text("예약", style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: SafeArea(
+          
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child:  Column(
+              children: [
+                Expanded(child: Container()),
+                Material(
+                  color: Colors.white,
+                  child: InkWell(
+                    onTap: () => print(1234),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 0.5,
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(2.0),
+                      ),
+                      height: 100,
+                      child: Row(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: SizedBox(
+                              width: 30,
+                              height: 30,
+                              child: Image.asset('images/icon_calender.png'),
+                            ),
+                          ),
+                          Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('날짜선택', style: TextStyle(fontWeight: FontWeight.bold)),
+                                SizedBox(height: 10),
+                                Text('예약 원하시는 날짜가 있다면\n날짜를 먼저 선택해주세요.'),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              );
-            }
+                SizedBox(height: 10),
+                Material(
+                  color: Colors.white,
+                  child: InkWell(
+                    onTap: () => print(1234),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 0.5,
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(2.0),
+                      ),
+                      height: 100,
+                      child: Row(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: SizedBox(
+                              width: 30,
+                              height: 30,
+                              child: Image.asset('images/logo_smile.png'),
+                            ),
+                          ),
+                          Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('메뉴선택', style: TextStyle(fontWeight: FontWeight.bold)),
+                                SizedBox(height: 10),
+                                Text('구매 원하시는 메뉴가 있다면\n메뉴를 먼저 선택해주세요.'),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 100),
+                Expanded(child: Container()),
+                // HomeUserInfo(),
+                // BestMenu(),
+                // NoticeCarousel(),
+                // EventCarousel(),
+              ],
+            ),
           ),
         ),
       ),
